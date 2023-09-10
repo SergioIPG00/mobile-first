5e0be2e94c5a9df8280354671d2b91b0fe361db0
SergioIPG00 <ivansergio472@gmail.com>
2023-09-10 18:54:08 -0500

    Revert "cambio a vite con react"
    This reverts commit e146245005dd846428b4e24b396cd643071cfb34.

--- src/services/games-services.js
+++
@@ -1,9 +0,0 @@
-import data from "./games.json";
-
-export function getGames(){
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
-            resolve(data.games);
-        }, 3000);
-    });
-}
